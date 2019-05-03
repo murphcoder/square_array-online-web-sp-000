2c243468c452684141e7a2438c5541e0972b5e21
Sean Murphy <filmmakersean@fastmail.net>
2019-05-03 15:43:45 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,3 @@
 def square_array(array)
-  squared = Array.new
-  count = 0
-  array.each do |number|
-    squared[count] = number ** 2
-    count += 1
-  end
-  return squared
+  array.collect 
 end
